88e7dcc43d05ebd89213cdfef259d1d5f7789797
Veda-369 <v.bharghav3@gmail.com>
2025-03-14 00:12:46 -0400

    script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Smooth Scrolling
-    document.querySelectorAll('nav ul li a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetElement.offsetTop - 50,
-                behavior: 'smooth'
-            });
-        });
-    });
-
     // Dark/Light Mode Toggle
     const themeToggle = document.getElementById("theme-toggle");
 
